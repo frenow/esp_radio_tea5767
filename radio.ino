11f612dfd27812c943f42d6c1938c69fa4f2fe1c
Emerson <frenow@gmail.com>
2024-11-05 14:26:39 -0300

    fix readme

--- radio.ino
+++ radio.ino
@@ -1,37 +1,3 @@
-///
-/// \file  TestTEA5767.ino
-/// \brief An Arduino sketch to operate a TEA5767 chip based radio using the Radio library.
-///
-/// \author Matthias Hertel, http://www.mathertel.de
-/// \copyright Copyright (c) 2014 by Matthias Hertel.\n
-/// This work is licensed under a BSD style license.\n
-/// See http://www.mathertel.de/License.aspx
-///
-/// \details
-/// This sketch implements a "as simple as possible" radio without any possibility to modify the settings after initializing the chip.\n
-/// The radio chip is initialized and setup to a fixed band and frequency. These settings can be changed by modifying the 
-/// FIX_BAND and FIX_STATION definitions. 
-///
-/// Open the Serial console with 57600 baud to see the current radio information.
-///
-/// Wiring
-/// ------ 
-/// Arduino port | TEA5767 signal
-/// ------------ | ---------------
-///         3.3V | VCC
-///          GND | GND
-///           A5 | SCLK
-///           A4 | SDIO
-///           D2 | RST
-///
-/// More documentation is available at http://www.mathertel.de/Arduino
-/// Source Code is available on https://github.com/mathertel/Radio
-///
-/// History:
-/// --------
-/// * 15.09.2014 created.
-/// * 15.11.2015 wiring corrected.
-
 #include <Arduino.h>
 #include <Wire.h>
 #include <radio.h>
